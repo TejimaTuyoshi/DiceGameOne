78a489feed56ac165494cf09882ede9bad6d8bc2
LIFE4649\tejim <tejima.tuyoshi@icloud.com>
2024-11-16 16:01:55 +0900

    初期設定

---
+++ OriginalGameOne/Assets/Script/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player:MonoBehaviour
+{
+    [SerializeField] GameObject Object;
+    void Start()
+    {
+       Debug.Log("start ok");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
